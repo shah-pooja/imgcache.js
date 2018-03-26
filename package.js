67379a1def032e06a69ebacca36d046b0cdf92c0
shah-pooja <31841740+shah-pooja@users.noreply.github.com>
2018-03-26 15:32:14 +0530

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "JS library that stores images locally for offline apps using PhoneGap/Cordova or browsers supporting the new html5 File API",
   version: "2.0.0",
-  git: "https://github.com/chrisben/imgcache.js"
+  git: "https://github.com/shah-pooja/imgcache.js"
 });
 
 Package.onUse(function(api) {
